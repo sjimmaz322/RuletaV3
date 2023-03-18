e9da95338e048bf0b7a2f3c62e035fdc5f436ac7
sjimmaz322 <sjimmaz322@g.educaand.es>
2023-03-18 20:41:26 +0100

    Otra versión de la ruleta

---
+++ Ruleta con animación/script.js
@@ -0,0 +1,38 @@
+const roulette = document.querySelector("#roulette");
+const spinButton = document.querySelector("#spin")
+const resetButton = document.querySelector("#reset")
+
+const maxSpins = 10;
+const minSpins = 1;
+
+const maxDegrees = 360;
+const minDegrees = 15;
+
+const getRandomNumber = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+spinButton.addEventListener("click", () => {
+
+    const spins = getRandomNumber(minSpins, maxSpins);
+    const degrees = getRandomNumber(minDegrees, maxDegrees);
+
+    const fullSpins = (spins - 1) * 360;
+    const spin = fullSpins + degrees;
+
+    const animationTime = spins;
+
+    roulette.style.transform = `rotate(${spin}deg)`;
+    roulette.style.transitionDuration = `${animationTime}s`;
+
+    spinButton.style.display = "none";
+    resetButton.style.display = "inline-block";
+
+});
+
+resetButton.addEventListener("click", () => {
+    roulette.style.transform = "rotate(0deg)";
+    roulette.style.transitionDuration = "1.5s";
+    spinButton.style.display = "inline-block";
+    resetButton.style.display = "none";
+});
