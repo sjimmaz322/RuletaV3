f6f0f4d7a009cbc01af76365e32fac42c1c1ae7b
sjimmaz322 <sjimmaz322@g.educaand.es>
2023-03-19 20:45:55 +0100

    Realizadas las pruebas del 1 al 7

--- Ruleta con animación/script.js
+++ Ruleta con animación/script.js
@@ -60,28 +60,155 @@ spinButton.addEventListener("click", () => {
 
     const numPrueba = getRandomNumber(1, 10);
 
+    // ROJO - BESAR
     if (result == 1 && numPrueba == 1) {
-        prueba = "beso en los labios";
+        prueba = "Dar un beso en la mejilla.";
     } else if (result == 1 && numPrueba == 2) {
-        prueba = "beso en la mejilla"
+        prueba = "Dar un beso en la mano."
     } else if (result == 1 && numPrueba == 3) {
-        prueba = "beso en el cuello"
+        prueba = "Dar un beso en la frente."
     } else if (result == 1 && numPrueba == 4) {
-        prueba = "beso en el pecho"
+        prueba = "Dar un beso en la barriga."
     } else if (result == 1 && numPrueba == 5) {
-        prueba = "beso en la barriga"
+        prueba = "Dar un beso en el cuello."
     } else if (result == 1 && numPrueba == 6) {
-        prueba = "beso en la frente"
+        prueba = "Dar un beso donde el jugador quiera."
     } else if (result == 1 && numPrueba == 7) {
-        prueba = "beso en el interior del muslo"
+        prueba = "Dar un beso en los labios."
     } else if (result == 1 && numPrueba == 8) {
-        prueba = "beso en las nalgas"
+        prueba = "Dar un beso en las nalgas."
     } else if (result == 1 && numPrueba == 9) {
-        prueba = "beso en la mano"
+        prueba = "Dar un beso en el pecho."
     } else if (result == 1 && numPrueba == 10) {
-        prueba = "beso donde quieras"
+        prueba = "Dar un beso en el interior del muslo."
+        // VERDE - MASAJEAR
+    } else if (result == 2 && numPrueba == 1) {
+        prueba = "Masajea sensualmente el pelo.";
+    } else if (result == 2 && numPrueba == 2) {
+        prueba = "Masajea sensualmente la cara."
+    } else if (result == 2 && numPrueba == 3) {
+        prueba = "Masajea sensualmente el cuello."
+    } else if (result == 2 && numPrueba == 4) {
+        prueba = "Masajea sensualmente la espalda."
+    } else if (result == 2 && numPrueba == 5) {
+        prueba = "Masajea sensualmente el pecho."
+    } else if (result == 2 && numPrueba == 6) {
+        prueba = "Masajea sensualmente las piernas."
+    } else if (result == 2 && numPrueba == 7) {
+        prueba = "Masajea sensualmente los brazos."
+    } else if (result == 2 && numPrueba == 8) {
+        prueba = "Masajea sensualmente las nalgas."
+    } else if (result == 2 && numPrueba == 9) {
+        prueba = "Masajea sensualmente lo que quieras."
+    } else if (result == 2 && numPrueba == 10) {
+        prueba = "Masaje sensualmente la entrepierna."
+        // AZUL - POSTURA SEXUAL
+    } else if (result == 3 && numPrueba == 1) {
+        prueba = "Postura sexual: El 69.";
+    } else if (result == 3 && numPrueba == 2) {
+        prueba = "Postura sexual: El misionero."
+    } else if (result == 3 && numPrueba == 3) {
+        prueba = "Postura sexual: Doggy Style. (El perrito)"
+    } else if (result == 3 && numPrueba == 4) {
+        prueba = "Postura sexual: La amazona."
+    } else if (result == 3 && numPrueba == 5) {
+        prueba = "Postura sexual: La cucharita."
+    } else if (result == 3 && numPrueba == 6) {
+        prueba = "Postura sexual: Duro contra el muro."
+    } else if (result == 3 && numPrueba == 7) {
+        prueba = "Postura sexual: La carretilla."
+    } else if (result == 3 && numPrueba == 8) {
+        prueba = "Postura sexual: Sexo Oral (A elección del jugador)"
+    } else if (result == 3 && numPrueba == 9) {
+        prueba = "Postura sexual: El cabecero. (Alguien se va a sentar en la cara de otro alguien)"
+    } else if (result == 3 && numPrueba == 10) {
+        prueba = "Postura sexual: A elección del jugador."
+        // AMARILLO - MOSTRAR
+    } else if (result == 4 && numPrueba == 1) {
+        prueba = "Hora de enseñar: La barriga.";
+    } else if (result == 4 && numPrueba == 2) {
+        prueba = "Hora de enseñar: El pecho."
+    } else if (result == 4 && numPrueba == 3) {
+        prueba = "Hora de enseñar: Las nalgas"
+    } else if (result == 4 && numPrueba == 4) {
+        prueba = "Hora de enseñar: Los pies"
+    } else if (result == 4 && numPrueba == 5) {
+        prueba = "Hora de enseñar: Las piernas."
+    } else if (result == 4 && numPrueba == 6) {
+        prueba = "Hora de enseñar: Enséñalo todo"
+    } else if (result == 4 && numPrueba == 7) {
+        prueba = "Hora de enseñar: No tienes que enseñar nada. (Te libraste)"
+    } else if (result == 4 && numPrueba == 8) {
+        prueba = "Hora de enseñar: El pubis (solo el pubis)"
+    } else if (result == 4 && numPrueba == 9) {
+        prueba = "Hora de enseñar: Los pezones"
+    } else if (result == 4 && numPrueba == 10) {
+        prueba = "Hora de enseñar: Lo que quiera enseñar el jugador."
+        // MARRÓN - CAMBIO DE ROPA
+    } else if (result == 5 && numPrueba == 1) {
+        prueba = "Intercambia tu sudadera/chaqueta.";
+    } else if (result == 5 && numPrueba == 2) {
+        prueba = "Intercambia tu camisa/camiseta."
+    } else if (result == 5 && numPrueba == 3) {
+        prueba = "Intercambia tu pantalón/falda."
+    } else if (result == 5 && numPrueba == 4) {
+        prueba = "Intercambia tus calcetines/medias."
+    } else if (result == 5 && numPrueba == 5) {
+        prueba = "Intercambia tu conjunto entero."
+    } else if (result == 5 && numPrueba == 6) {
+        prueba = "Intercambia... nada, te queda bien tu modelito."
+    } else if (result == 5 && numPrueba == 7) {
+        prueba = "Intercambia la prenda que tú quieras."
+    } else if (result == 5 && numPrueba == 8) {
+        prueba = "Intercambia tu camiseta/camisa."
+    } else if (result == 5 && numPrueba == 9) {
+        prueba = "Intercambia tu falda/pantalón."
+    } else if (result == 5 && numPrueba == 10) {
+        prueba = "Intercambia la prenda que tú quieras."
+        // ROSA - MORDER
+    } else if (result == 6 && numPrueba == 1) {
+        prueba = "Muerde una mejilla.";
+    } else if (result == 6 && numPrueba == 2) {
+        prueba = "Muerde los labios."
+    } else if (result == 6 && numPrueba == 3) {
+        prueba = "Muerde en el cuello."
+    } else if (result == 6 && numPrueba == 4) {
+        prueba = "Muerde un brazo."
+    } else if (result == 6 && numPrueba == 5) {
+        prueba = "Muerde un muslo."
+    } else if (result == 6 && numPrueba == 6) {
+        prueba = "Muerde el pecho."
+    } else if (result == 6 && numPrueba == 7) {
+        prueba = "Muerde la barriga."
+    } else if (result == 6 && numPrueba == 8) {
+        prueba = "Muerde lo que quieras."
+    } else if (result == 6 && numPrueba == 9) {
+        prueba = "Muerde una nalga... o las dos."
+    } else if (result == 6 && numPrueba == 10) {
+        prueba = "Muerde el pubis."
+        // NEGRO - QUITARSE
+    } else if (result == 7 && numPrueba == 1) {
+        prueba = "Se va: Tu sudadera/chaqueta.";
+    } else if (result == 7 && numPrueba == 2) {
+        prueba = "Se va: Tu camiseta/camisa."
+    } else if (result == 7 && numPrueba == 3) {
+        prueba = "Se va: Tu pantalón/falda."
+    } else if (result == 7 && numPrueba == 4) {
+        prueba = "Se va: Tus zapatillas."
+    } else if (result == 7 && numPrueba == 5) {
+        prueba = "Se va: Tus calcetines."
+    } else if (result == 7 && numPrueba == 6) {
+        prueba = "Se va: Todo de cintura para arriba."
+    } else if (result == 7 && numPrueba == 7) {
+        prueba = "Se va: Todo"
+    } else if (result == 7 && numPrueba == 8) {
+        prueba = "Se va: Nada, todo se queda."
+    } else if (result == 7 && numPrueba == 9) {
+        prueba = "Se va: Lo que quieras quitarte."
+    } else if (result == 7 && numPrueba == 10) {
+        prueba = "Se va: Todo de cintura para abajo."
     } else {
-        prueba = "texto de ejemplo"
+        prueba = "Undefined at the moment"
     }
 
 
@@ -116,7 +243,7 @@ const lista = document.querySelector('#lista-participantes');
 const botonAgregar = document.querySelector('#agregar');
 
 botonAgregar.addEventListener('click', () => {
-    // Añadir el nombre a la lista
+
     const nombre = input.value;
     participantes.push(nombre);
     const itemLista = document.createElement('li');
@@ -124,7 +251,6 @@ botonAgregar.addEventListener('click', () => {
     itemLista.textContent = nombre;
     lista.appendChild(itemLista);
 
-    // Vaciar el campo de texto
     input.value = '';
 });
 
